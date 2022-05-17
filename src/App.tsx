913a86a7189c8a79fc19caed49b7908bc2de24f8
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-05-17 18:30:00 +0400

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
+"use strict"; 
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  var name:string = "John"; 
+  var score1:number = 50;
+  var score2:number = 42.50;
+  // console.log("Typeof name:string",typeof (name));
+  // console.log("Typeof name:number",typeof (score1));
+
+  var str = '1';
+  var str2 = str;
+  // console.log(typeof (str), typeof (str2));  
+
+  let num = 2;  
+  // console.log("value of num "+num); 
+  num = 12;
+  // console.log(num);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Type Script</h1>
+      <h2>Variable Declaration Syntax</h2>
+      <p>var name:string = ”mary”</p>
+      <p>var name:string;</p>
+      <p>var name = ”mary”</p>
+      <p>var name;</p>
     </div>
   );
 }
