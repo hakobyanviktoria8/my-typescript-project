836c3e94b7034b2adff8136c7d5ee3daaea7a39b
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-05-18 13:42:18 +0400

    work loop and functions

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,71 @@
 "use strict"; 
 import React from 'react';
 
-function add(a:number,b:number) {
-  return a+b
-}
-console.log(add(2,3));
 
 function App() {
-  var name:string = "John"; 
-  var score1:number = 50;
-  var score2:number = 42.50;
-  // console.log("Typeof name:string",typeof (name));
-  // console.log("Typeof name:number",typeof (score1));
+  // function add(a:number,b:number) {
+  //   return a+b
+  // }
+  // // console.log(add(2,3));
 
-  var str = '1';
-  var str2 = str;
-  // console.log(typeof (str), typeof (str2));  
+//   var name:string = "John"; 
+//   var score1:number = 50;
+//   var score2:number = 42.50;
+//   // console.log("Typeof name:string",typeof (name));
+//   // console.log("Typeof name:number",typeof (score1));
 
-  let num = 2;  
-  // console.log("value of num "+num); 
-  num = 12;
-  // console.log(num);
+//   var str = '1';
+//   var str2 = str;
+//   // console.log(typeof (str), typeof (str2));  
 
+//   // let num = 2;  
+//   // console.log("value of num "+num); 
+//   // num = 12;
+//   // console.log(num);
+// let bool:boolean = true
+// console.log(typeof (bool), bool)
+// bool = ""?true:false
+
+// var numm:number = -2 
+// var result = numm > 0 ?"positive":"non-positive" 
+// console.log(result)
+
+// function greet(person: string, date: Date) {
+//   console.log(`Hello ${person}, today is ${date.toDateString()}!`);
+// }
+ 
+// greet("Maddison", new Date());
+
+// var num:number = 0
+// var count:number = 0;
+
+// for(num=0;num<=20;num++) {
+//    if (num % 2==0) {
+//       continue
+//    }
+//    count++
+// }
+// console.log (" The count of odd values between 0 and 20 is: "+count) 
+
+// function addNumbers(...nums:number[]) {  
+//   var i;   
+//   var sum:number = 0; 
+  
+//   for(i = 0;i<nums.length;i++) { 
+//      sum = sum + nums[i]; 
+//   } 
+//   console.log("sum of the numbers",sum) 
+// } 
+// addNumbers(1,2,3) 
+// addNumbers(10,10,10,10,10)
+
+var person = { 
+  firstname:"Tom", 
+  lastname:"Hanks" 
+}; 
+//access the object values 
+console.log(person.firstname) 
+console.log(person.lastname)
 
   return (
     <div className="App">
