178b9ca8765bcd1a1384f5418bb01a1f3111fcb1
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-06-03 21:30:16 +0400

    work with function

--- src/Add.tsx
+++ src/Add.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
 
 function Add() {
+  // we can make type
+  type Combine = number|string
+
+  const combine=(
+    input1: Combine,
+    input2: Combine
+  )=>{
+    const result = +input1 + +input2;
+    return result
+  }
   let a:number=2;
   let b:number=3;
+
+  let add = combine
+  console.log(add(2,2));
   
   return (
-    <div>Sum a + b = {a+b}</div>
+    <div>
+      <h3>Sum a + b = {a+b}</h3>
+      <p>{combine(9,8)}</p>
+       {/* <p>{combine("Tom",8)}</p> NaN */}
+    </div>
   )
 }
 
